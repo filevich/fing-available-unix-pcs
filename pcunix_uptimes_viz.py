69f82c496b6c5e6765de1b58e438da4d105c4918
J.P. Filevich <juan.filevich@fing.edu.uy>
2025-06-07 23:39:15 -0300

    Add additional CSV file for uptime heatmap plotting

--- pcunix_uptimes_viz.py
+++ pcunix_uptimes_viz.py
@@ -125,6 +125,7 @@ csv_files_to_plot = [
     '/tmp/pcunix_nodes_reachability_20250607_185820.csv',
     '/tmp/pcunix_nodes_reachability_20250607_190750.csv',
     '/tmp/pcunix_nodes_reachability_20250607_193422.csv',
+    '/tmp/pcunix_nodes_reachability_20250607_202154.csv'
 ]
 
 plot_uptime_heatmap(csv_files_to_plot)
